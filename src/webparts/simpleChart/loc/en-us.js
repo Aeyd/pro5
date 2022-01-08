a4023607a9574a5bd7b8cdc347e660d6831ab009
thomasbuchinger <s1910238009@students.fh-hagenber.at>
2022-01-08 17:05:42 +0100

    aa

--- src/webparts/simpleChart/loc/en-us.js
+++ src/webparts/simpleChart/loc/en-us.js
@@ -2,6 +2,33 @@ define([], function() {
   return {
     "PropertyPaneDescription": "Description",
     "BasicGroupName": "Group Name",
-    "DescriptionFieldLabel": "Description Field"
+    "DescriptionFieldLabel": "Description Field",
+    "SortFieldLabel": "Sort",
+    "OptionsFieldLabel": "max Items",
+    "ColorFieldLabel": "Colors",
+    "SortFileUnsorted": "unsorted",
+    "SortFileAsc": "ascending data",
+    "SortFileDesc": "descending data",
+    "SortFileAscD": "ascending labels",
+    "SortFileDescD": "descending labels",
+    "ColorFieldStandard": "Standard",
+    "ColorFieldMonochrome": "Monochrome",
+    "ColorFieldColor": "Colorful",
+    "ModeFieldLabel": "Mode",
+    "ModeFieldNormal": "Normal",
+    "ModeFieldCount": "Count",
+    "ModeFieldGroup": "Group by Count",
+    "GeneralFieldLabel": "General",
+    "DataGroupName": "Data",
+    "ModeFieldList": "List",
+    "LabelFieldLabel": "Label Column",
+    "DataFieldLabel": "Data Column",
+
+
+
+
+
+
+
   }
 });
--- src/webparts/simpleChart/loc/mystrings.d.ts
+++ src/webparts/simpleChart/loc/mystrings.d.ts
@@ -2,6 +2,26 @@ declare interface ISimpleChartWebPartStrings {
   PropertyPaneDescription: string;
   BasicGroupName: string;
   DescriptionFieldLabel: string;
+  SortFieldLabel: string;
+  OptionsFieldLabel: string;
+  ColorFieldLabel: string;
+  ModeFieldLabel: string;
+  SortFileUnsorted:  string;
+  SortFileAsc: string;
+  SortFileDesc: string;
+  SortFileAscD: string;
+  SortFileDescD: string;
+  ColorFieldStandard: string;
+  ColorFieldMonochrome: string;
+  ColorFieldColor: string;
+  ModeFieldNormal: string;
+  ModeFieldCount: string;
+  ModeFieldGroup: string;
+  GeneralFieldLabel: string;
+  DataGroupName: string;
+  ModeFieldList: string;
+  LabelFieldLabel: string;
+  DataFieldLabel: string;
 }
 
 declare module 'SimpleChartWebPartStrings' {
